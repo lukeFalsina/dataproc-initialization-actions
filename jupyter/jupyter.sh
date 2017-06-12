a94296f73e27ab9a64cd1eb33960dc2c666ea883
Luca Falsina <lfalsina@gmail.com>
2017-06-12 14:28:37 +0200

    Jupyter.sh: Setup sparkling water with Scala and Python APIs
    A modification to the original script to have H2O sparkling water
    available both in spark-submit and jupyter notebooks.

--- jupyter/jupyter.sh
+++ jupyter/jupyter.sh
@@ -30,7 +30,29 @@ if [ -n "${JUPYTER_CONDA_PACKAGES}" ]; then
   conda install $(echo ${JUPYTER_CONDA_PACKAGES} | tr ':' ' ')
 fi
 
+pip install h2o_pysparkling_2.0
+
 if [[ "${ROLE}" == 'Master' ]]; then
+    # Sparkling water setup
+    SPARKLING_WATER_MAJOR=$(curl -f -s -H Metadata-Flavor:Google http://metadata/computeMetadata/v1/instance/attributes/SPARKLING_WATER_MAJOR || true)
+    SPARKLING_WATER_MAJOR="${SPARKLING_WATER_MAJOR:-2.0}"
+
+    SPARKLING_WATER_MINOR=$(curl -f -s -H Metadata-Flavor:Google http://metadata/computeMetadata/v1/instance/attributes/SPARKLING_WATER_MINOR || true)
+    SPARKLING_WATER_MINOR="${SPARKLING_WATER_MINOR:-9}"
+
+    SPARKLING_WATER_VERSION="${SPARKLING_WATER_MAJOR}.${SPARKLING_WATER_MINOR}"
+    SPARKLING_WATER_URL="http://h2o-release.s3.amazonaws.com/sparkling-water/rel-${SPARKLING_WATER_MAJOR}/${SPARKLING_WATER_MINOR}/sparkling-water-${SPARKLING_WATER_VERSION}.zip"
+    HADOOP_VERSION=hdp2.6
+
+    echo "Retrieving Sparkling Water (version ${SPARKLING_WATER_VERSION})"
+    wget ${SPARKLING_WATER_URL}
+    unzip sparkling-water-${SPARKLING_WATER_VERSION}.zip
+    echo "Create extended JAR for Hadoop version ${HADOOP_VERSION}"
+    ./sparkling-water-${SPARKLING_WATER_VERSION}/bin/get-extended-h2o.sh ${HADOOP_VERSION}
+
+    echo "Make H2O extended JAR available to the environment and Spark"
+    echo "H2O_EXTENDED_JAR=`pwd`/h2odriver-${HADOOP_VERSION}-extended.jar" | tee -a /etc/environment /usr/lib/spark/conf/spark-env.sh
+
     conda install jupyter
     if gsutil -q stat "gs://$DATAPROC_BUCKET/notebooks/**"; then
         echo "Pulling notebooks directory to cluster master node..."
@@ -38,6 +60,7 @@ if [[ "${ROLE}" == 'Master' ]]; then
     fi
     ./dataproc-initialization-actions/jupyter/internal/setup-jupyter-kernel.sh
     ./dataproc-initialization-actions/jupyter/internal/launch-jupyter-kernel.sh
+
 fi
 echo "Completed installing Jupyter!"
 
